25b2e5387a9f729e8c4fbfcfd7bf79fd11c07311
Karl-Erik Réculé <karlerik.recule@gmail.com>
2021-11-15 21:13:30 +0100

    Add game and score model

--- server/app/models/game.rb
+++ server/app/models/game.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-class User < ApplicationRecord
+class Game < ApplicationRecord
   has_many: :score
 
-  validates :name,
+  validates :status,
             presence: true
 end
---
+++ server/app/models/score.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Score < ApplicationRecord
+  belongs_to: :game
+  belongs_to: :user
+
+  validates :taps, presence: true
+  validates :colour, presence: true
+end
