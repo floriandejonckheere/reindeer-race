5a99ffe06cc61381bd13f5e700046a2a4a608e64
Florian Dejonckheere <florian@nephroflow.com>
2021-11-15 21:20:05 +0100

    Update db schema

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,10 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_05_211659) do
+ActiveRecord::Schema.define(version: 2021_11_15_183910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string "status", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "scores", force: :cascade do |t|
+    t.integer "users_id", null: false
+    t.integer "games_id", null: false
+    t.string "colour", null: false
+    t.datetime "score"
+    t.integer "taps", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["games_id"], name: "index_scores_on_games_id"
+    t.index ["users_id"], name: "index_scores_on_users_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -21,4 +41,6 @@ ActiveRecord::Schema.define(version: 2021_11_05_211659) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "scores", "games", column: "games_id", on_delete: :cascade
+  add_foreign_key "scores", "users", column: "users_id", on_delete: :cascade
 end
