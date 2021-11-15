651981918dc396b06bd8f16aa7db17ed7fe5c8ff
Karl-Erik Réculé <karlerik.recule@gmail.com>
2021-11-15 22:43:05 +0100

    Add game/:id/user and game/ endpoints

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -2,4 +2,9 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :game, only: [:index] do
+    member do
+      post :user
+    end
+  end
 end
