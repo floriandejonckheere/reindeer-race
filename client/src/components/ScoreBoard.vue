5753b95d3d0d8ce42a109846eb0e40406c409f0d
Ayrton Vercruysse <ayrton.vercruysse@niprodigital.com>
2021-11-15 22:22:46 +0100

    Create scoreboards

---
+++ client/src/components/ScoreBoard.vue
@@ -0,0 +1,65 @@
+<template>
+<table class="customer">
+  <header>
+    <th></th>
+    <th>NickName</th>
+    <th>Time</th>
+  </header>
+  <body>
+    <tr v-for="score,idx in scores" :key="idx">
+      <td>{{ idx }}</td>
+      <td>{{ score.username }}</td>
+      <td>{{ score.score }}</td>
+    </tr>
+  </body>
+</table>
+</template>
+
+<script setup lang="ts">
+import { defineProps, onMounted } from "vue"
+const props = defineProps({
+  type: String,
+  id: Number
+})
+let scores = $ref<Response["scores"]>([])
+console.log(props)
+interface Response { scores: Score[] }
+interface Score { username: string, score: number }
+
+onMounted(() => {
+  getScores()
+})
+
+async function getScores() {
+  // const endpoint = import.meta.env.VITE_SERVER_URL;
+  // const better_endpoint = props.type === "gameResult" ? `${endpoint}/game/${props.id}/score` : `${endpoint}/leaderboard`
+  // const response = await fetch(better_endpoint)
+  // const data:Response = await response.json()
+  const data = {
+    scores: [
+      {
+        username: "Jasper",
+        score: 23456
+      },
+      {
+        username: "Karl-Erik",
+        score: 23456
+      },
+      {
+        username: "Pieter",
+        score: 23456
+      },
+      {
+        username: "Florian",
+        score: 23456
+      },
+      {
+        username: "Yannick",
+        score: 23456
+      }
+    ]
+  }
+
+  scores = data.scores
+}
+</script>
--- client/src/pages/projected/Leaderboard.vue
+++ client/src/pages/projected/Leaderboard.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>Leaderboard</div>
+  <div>
+    <h1>Leaderboard</h1>
+  </div>
+  <div>
+    <ScoreBoard type="gameResult" :id="gameId"></ScoreBoard>
+  </div>
 </template>
 
 <script setup lang="ts">
-//
+import ScoreBoard from '../../components/ScoreBoard.vue';
+const gameId = 1
 </script>
+
+<style>
+h1 {
+  text-align: center;
+  color: red;
+}
+</style>
