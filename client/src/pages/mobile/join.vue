cf4b40b95bce6327f20d146f2f4f3af6f51dac29
Pieter Vercruyce <pieter.vercruyce@niprodigital.com>
2021-11-22 23:15:47 +0100

    join page

--- client/src/pages/mobile/join.vue
+++ client/src/pages/mobile/join.vue
@@ -1,48 +1,83 @@
 <template>
-  <link
-    href="https://fonts.googleapis.com/css?family=Mountains of Christmas"
-    rel="stylesheet"
-  />
+  <link href="https://fonts.googleapis.com/css?family=Mountains of Christmas" rel="stylesheet" />
   <h1>REINDEER RACE</h1>
   <form ref="scheme_form">
-    <p>{{ name }}</p>
     <label for="name">Gebruikersnaam:</label>
     <br />
-    <input v-model="name" type="text" name="name" />
+    <input v-model="name" type="text" name="name" id="name_field" />
+    <br />
+
+    <div class="radio-toolbar">
+      <div v-for="(colour_active, colour_name) in colours">
+        <input v-if="colour_active" type="radio" id="radio-button" name="radioFruit" value="apple" />
+        <label
+          v-if="colour_active"
+          v-on:click="radio_button_clicked(colour_name)"
+          v-bind:class="colour_name"
+          for="radio-button"
+          class="radio-button-label"
+        ></label>
+      </div>
+    </div>
+
     <br />
     <button type="button" @click="join_button">Join</button>
   </form>
 </template>
 
-<script setup>
-import { onBeforeUnmount, onMounted } from "vue";
+<script setup lang="ts">import { onBeforeUnmount, onMounted } from '@vue/runtime-core'
 
-let name = $ref("");
-let error = $ref("");
-let colours = $ref([]);
-let game_id = $ref("");
-let interval = $ref(null);
+let name = $ref('')
+let error = $ref('')
+let colours = $ref('')
+let game_id = $ref('')
+let interval = $ref<number | null>(null)
+let selected_colour = $ref('')
 
 onMounted(() => {
-  interval = setInterval(fetchGame, 1000);
-});
+  // interval = setInterval(fetchGame, 1000)
+  fetchGame()
+})
 
 onBeforeUnmount(() => {
-  if (interval) clearInterval(interval);
-});
+  if (interval) clearInterval(interval)
+})
+
+function radio_button_clicked(colour_name) {
+  var radio_button_labels = document.getElementsByClassName("radio-button-label")
+  console.log(radio_button_labels)
+  for (var label of radio_button_labels) {
+    label.classList.remove("selected-radio-button")
+  }
+  selected_colour = colour_name
+  document.getElementsByClassName(selected_colour)[0].classList.add("selected-radio-button")
+}
 
 async function fetchGame() {
-  const get_game = `${import.meta.env.SERVER_URL}/game`;
-  const response = await fetch(get_game);
-  const data = await response.json();
+  const get_game = `${import.meta.env.VITE_SERVER_URL}/game`
+  const response = await fetch(get_game)
+  const data = await response.json()
 
-  game_id = data.game_id;
-  colours = data.colours;
+  game_id = data.game_id
+  colours = data.colours
 }
 
 async function join_button() {
-  const join_game = `${import.meta.env.SERVER_URL}/game/${game_id}/user`;
+  var post_data = { "name": name, "colour": selected_colour }
+  console.log(post_data)
+  const join_game = `${import.meta.env.VITE_SERVER_URL}/game/${game_id}/user`
+  const response = await fetch(join_game, {
+    method: 'POST',
+    mode: 'cors',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(post_data)
+  })
+
 }
+
 </script>
 
 <style scoped>
@@ -74,7 +109,7 @@ form {
   left: 30%;
 }
 
-input {
+#name_field {
   width: 80%;
   height: 30px;
   text-align: center;
@@ -102,5 +137,62 @@ img {
 
 html {
   background-color: aqua;
+}
+
+.radio-toolbar input[type="radio"] {
+  opacity: 0;
+  position: fixed;
+  width: 0;
+}
+
+.radio-toolbar div {
+  display: inline-block;
+  margin-top: 5px;
+}
+
+.radio-button-label {
+  padding: 1px 10px;
+  border: 0px solid;
+  border-radius: 10px;
+  margin-right: 2px;
+  margin-left: 2px;
+}
+
+.red {
+  background-color: red;
+}
+
+.blue {
+  background-color: blue;
+}
+
+.green {
+  background-color: green;
+}
+
+.purple {
+  background-color: purple;
+}
+
+.yellow {
+  background-color: yellow;
+}
+
+.orange {
+  background-color: orange;
+}
+
+.pink {
+  background-color: pink;
+}
+
+.gray {
+  background-color: gray;
+}
+
+.selected-radio-button {
+  border: 2px solid black !important;
+  margin-right: 0px !important;
+  margin-left: 0px !important;
 }
 </style>
---
+++ server/app/controllers/game_controller.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+class GameController < ApplicationController
+  def index
+    # will test and clean this up in the next hackaton
+    waiting_lobby_games = Score
+      .joins(:game)
+      .where(game: { status: :waiting })
+      .group_by(:game_id)
+
+    waiting_loby = waiting_lobby_games.find { |game| game.size < 8 }
+
+    if waiting_loby.nil?
+      waiting_loby = Game.create!
+
+      colours = Game::COLOURS.each_with_object({}) do |colour, _hash|
+        hsh[colour] = true
+      end
+
+    else
+      used_colours = Score.where(game_id: waiting_loby.id).pluck(:colour)
+      unused_colours Game::COLOURS - used_colours
+
+      _used_colours_hash = used_colours.each_with_object({}) do |colour, _hash|
+        hsh[colour] = true
+      end
+
+      _unused_colours_hash = unused_colours.each_with_object({}) do |colour, _hash|
+        hsh[colour] = false
+      end
+
+      colours = used_colours.merge(unused_colours)
+
+    end
+    payload = {
+      colours: colours,
+      game_id: waiting_loby.id,
+    }
+
+    render json: payload
+  end
+
+  def user
+    ActiveRecord::Base.transaction do
+      user = User.create!(create_user_params.fetch(:name))
+
+      Score.create!({
+                      user: user,
+                      game: Game.find(params[:game_id]),
+                      colour: create_user_params.fetch(:colour),
+                    })
+    end
+
+    render json: user, status: :created
+  end
+
+  private
+
+  def create_user_params
+    params
+      .permit(
+        :name,
+        :colour,
+      )
+  end
+end
