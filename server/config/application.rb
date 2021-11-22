ddc40dbdafd2db17ace4c52a2eeefd2ee83d9814
Florian Dejonckheere <florian@nephroflow.com>
2021-11-22 17:47:32 +0100

    Enable ActiveJob

--- server/config/application.rb
+++ server/config/application.rb
@@ -5,7 +5,7 @@ require_relative "boot"
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-# require "active_job/railtie"
+require "active_job/railtie"
 require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
@@ -33,5 +33,6 @@ module Reindeer
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_job.queue_adapter = :sidekiq
   end
 end
