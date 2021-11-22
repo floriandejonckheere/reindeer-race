56a9842467dc830567ce9c7f6ed93979e277f0ca
Florian Dejonckheere <florian@nephroflow.com>
2021-11-22 18:09:55 +0100

    Add start game job

---
+++ server/app/jobs/start_job.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class StartJob < ApplicationJob
+  queue_as :default
+
+  def perform(*_args)
+    # Return if no lobby with players found
+    return unless game
+
+    game.transaction do
+      # Start game
+      game.update!(status: :ongoing, start_date: DateTime.current)
+
+      # Reset player stats
+      game.users.each { |user| user.stats.clear! }
+    end
+  end
+
+  private
+
+  def game
+    @game ||= Game
+      .where(status: :waiting)
+      .order(created_at: :desc)
+      .first
+  end
+end
