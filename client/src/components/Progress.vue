34a9e267a38842033e842b8774e017ca33d0c728
yvdewall <yvdewall@hotmail.com>
2021-11-22 19:22:32 +0100

    Add unstyled progress bar

--- client/src/components/Progress.vue
+++ client/src/components/Progress.vue
@@ -1,7 +1,50 @@
 <template>
-  <div>Progress</div>
+  <div class="progress">
+      <div class="marker"></div>
+  </div>
 </template>
 
 <script setup>
-//
+
+import {defineProps, watchEffect} from "vue";
+import { gsap } from "gsap";
+
+// props
+
+const props = defineProps({
+  markerDistance: {
+    type: Number,
+    required: true,
+    validator: (value) => {
+      if (value > 100 || value < 0) return false
+      return true
+    }
+  }
+})
+
+// watcher
+
+watchEffect(() => {
+  gsap.to('.progress .marker', {left: `calc(${props.markerDistance}% - 25px)`})
+})
+
 </script>
+
+<style>
+.progress {
+  width: 100%;
+  height: 20px;
+  background-color: black;
+  border-radius: 15px;
+  position: relative;
+}
+
+.marker {
+  height: 40px;
+  width: 40px;
+  border-radius: 40px;
+  background-color: red;
+  transform: translateY(-25%);
+  position: absolute;
+}
+</style>
--- client/src/pages/projected/game.vue
+++ client/src/pages/projected/game.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>Game</div>
+  <div style="width: 600px; margin: auto">
+    <Progress :marker-distance="markerDistance"></Progress>
+  </div>
 </template>
 
 <script setup>
-//
+import Progress from "@/components/Progress.vue";
+
+let markerDistance = $ref(0)
+
 </script>
