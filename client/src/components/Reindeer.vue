9b41e679e0a96ecdd3b2925e740379b142312adb
yvdewall <yvdewall@hotmail.com>
2021-11-22 21:07:00 +0100

    Add reindeer component Add progress bar component

--- client/src/components/Reindeer.vue
+++ client/src/components/Reindeer.vue
@@ -1,7 +1,61 @@
 <template>
-  <div>Reindeer</div>
+  <div class="container">
+    <img class="deer" :src="selectedDeer">
+  </div>
 </template>
 
 <script setup>
-//
+
+import {defineProps, watchEffect} from "vue";
+
+import deerOne from "@/assets/deers/deer_one.png"
+import deerTwo from "@/assets/deers/deer_two.png"
+import deerThree from "@/assets/deers/deer_three.png"
+import deerFour from "@/assets/deers/deer_four.png"
+import deerFive from "@/assets/deers/deer_five.png"
+
+const deers = [deerOne, deerTwo, deerThree, deerFour, deerFive]
+
+let selectedDeer = $ref(deerOne);
+let selectedDeerIndex = $ref(0)
+let interval = $ref(null)
+
+const props = defineProps({
+  running: {
+    type: Boolean,
+    required: true,
+  }
+})
+
+const startRunning = () => {
+  interval = setInterval(() => {
+    selectedDeerIndex += 1
+    if (selectedDeerIndex === 5) selectedDeerIndex = 0
+    selectedDeer = deers[selectedDeerIndex]
+    console.log(selectedDeer)
+  }, 100)
+}
+
+const stopRunning = () => {
+  clearInterval(interval)
+  selectedDeer = deerOne
+  selectedDeerIndex = 0
+}
+
+watchEffect(() => {
+  props.running ? startRunning() : stopRunning()
+})
+
+
 </script>
+
+<style>
+.container{
+  width:100px;
+  height: 100px;
+}
+
+.deer {
+  width: 100%;
+}
+</style>
--- client/src/pages/projected/game.vue
+++ client/src/pages/projected/game.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>Game</div>
+  <div style="width: 600px; margin: auto">
+    <Reindeer :running="running"/>
+    <Progress :marker-distance="markerDistance"/>
+  </div>
 </template>
 
 <script setup>
-//
+import Progress from "@/components/Progress.vue";
+import Reindeer from "@/components/Reindeer.vue"
+
+let markerDistance = $ref(0)
+let running = $ref(true)
+
 </script>
