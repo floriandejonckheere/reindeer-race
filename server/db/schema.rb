564981f031c4ef2d42e549daaecdb2c2f46b5902
Florian Dejonckheere <florian@nephroflow.com>
2021-11-22 17:40:22 +0100

    Add unique index on user name

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_183910) do
+ActiveRecord::Schema.define(version: 2021_11_22_173602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_11_15_183910) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
   add_foreign_key "scores", "games", column: "games_id", on_delete: :cascade
